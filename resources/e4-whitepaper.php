09ac4da57f9b7a8c3c46f1df0b07a4f53570e579
johna <johna>
2009-06-29 17:54:08 +0000

    Tinker

--- resources/e4-whitepaper.php
+++ resources/e4-whitepaper.php
@@ -1,7 +1,7 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	#
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "e4 technical overview";
+	$pageTitle 		= "Whitepaper: e4 Technical Overview";
 	$pageKeywords	= "Eclipse, e4, white paper";
 	$pageAuthor		= "John Arthorne";
 	
@@ -15,14 +15,24 @@
 	# Paste your HTML content between the markers!	
 ob_start();
 ?>		
-
+	<style>
+		p {
+			margin-left:20px;
+			line-height:150%;
+		}
+		ul {
+			margin-left:30px;
+		}
+	</style>
+<div id="maincontent">
 	<div id="midcolumn">
-		<h1>Whitepaper: e4 techical overview</h1>
+		<h1>$pageTitle</h1>
 
 		<h3><strong>Executive Summary</strong></h3>
 
 		<h3><strong>Another Section</strong></h3>
-
+	</div>
+</div?
 
 	<div id="rightcolumn">
 		<div class="sideitem">
